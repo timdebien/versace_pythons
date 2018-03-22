ce51db87b237914f57a45b1690a57e48869eac9e
Wyatt Sommer <wyatt@Wyatts-MacBook-Pro.local>
2018-03-21 18:28:12 -0600

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
-    $('.btn').on('click', function() {
+    $('.btn').on('click', function () {
         event.preventDefault();
 
         // store firstname in variable
@@ -13,16 +13,27 @@ $('document').ready(function() {
 
         // validate inputs
         if (name === '') {
-            
+            // must enter name
             alert('Must Enter Name');
         } else if (birthday === '') {
-            
+            // must enter birthday
             alert('Must Enter Birthday')
         } else {
+            // hide form
+            $('.form1').hide();
+            
+            // add text
+            $('#print-name').html('<h1>' + 'Hi ' + name + ', here is some info about your birthday!' + '</h1>').css('border-bottom', '2px solid #26a69a').addClass('animated slideInUp');
 
-            $('#print-name').html('<h1>' + 'Hi ' + name + ', Here is some info about your birthday!' + '</h1>')
         }
-
-        
     });
+
+    // var url = "https://api.nasa.gov/planetary/apod?api_key=TiviFiWBJyWVjtnQHT97G8Eg0q8ZaKRm0hOqDpk2";
+
+    // $.ajax({
+    //     url: url,
+    //     method: 'GET'
+    // }).then(function (response) {
+    //     console.log(response);
+    // });
 });
