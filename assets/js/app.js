f627d3848b293c5ccbe92c14bf819966475fbcfb
Wyatt Sommer <wyatt@Wyatts-MacBook-Pro.local>
2018-03-24 13:20:08 -0600

    wyatt weather

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,9 +11,13 @@ $('document').ready(function () {
         var lastname = $('#last-name').val();
         console.log(lastname);
 
-        // store job in variable
-        var inputLocation = $('#location').val();
-        console.log(inputLocation);
+        // store city in variable
+        var city = $('#city').val();
+        console.log(city);
+
+        // store state in variable
+        var state = $('#state').val();
+        console.log(state);
 
         // store job in variable
         var job = $('#job').val();
@@ -23,24 +27,29 @@ $('document').ready(function () {
         if (firstname === '') {
             // must enter first name
             $('#first-name').addClass('animated wobble');
-            $('#alert').text('*Must Enter First Name')
+            $('#alert').text('*Must Enter First Name');
         }
         else if (lastname === '') {
             // must enter last name
             $('#last-name').addClass('animated wobble');
-            $('#alert').text('*Must Enter Last Name')
+            $('#alert').text('*Must Enter Last Name');
         }
-        else if (inputLocation === '') {
-            // must enter location
-            $('#location').addClass('animated wobble');
-            $('#alert').text('*Must Enter Location')
+        else if (city === '') {
+            // must enter city
+            $('#city').addClass('animated wobble');
+            $('#alert').text('*Must Enter City');
+        }
+        else if (state === '') {
+            // must enter state
+            $('#state').addClass('animated wobble');
+            $('#alert').text('*Must Enter State');
         }
         else if (job === '') {
             // must enter job
             $('#job').addClass('animated wobble');
-            $('#alert').text('*Must Enter Job Description')
+            $('#alert').text('*Must Enter Job Description');
         }
-        else if (firstname !== '' && lastname !== '' && inputLocation !== '' && job !== '') {
+        else if (firstname !== '' && lastname !== '' && city !== '' && state !== '' && job !== '') {
 
             // hide form
             $('.form1').hide();
@@ -53,11 +62,13 @@ $('document').ready(function () {
 
             // add gif
             $('#gif').html('<img src=https://media.giphy.com/media/CTkWFZ1IDvsfS/giphy.gif>').addClass('animated slideInUp');
-            ///////////////////////mine
+            
             // This is our API key
-            var APIKey = "166a433c57516f51dfab1f7edaed8413";
+            var APIKey = "4679cf55751d13b3";
+
             // Here we are building the URL we need to query the database
-            var queryURL = queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + inputLocation + "&units=imperial&appid=" + APIKey;
+            var queryURL = "http://api.wunderground.com/api/" + APIKey + "/conditions/q/" + state + "/" + city + ".json"
+            
             // Here we run our AJAX call to the OpenWeatherMap API
             $.ajax({
                 url: queryURL,
@@ -69,16 +80,20 @@ $('document').ready(function () {
                     // log api data
                     console.log(response);
 
-                    $(".city").html("<h1>" + response.city.name + " Forecast Details</h1>");
-                    // $("#weather").html("<h1>" + " temperature </h1>" + response.list[0].temp.max);
+                    // store local temp
+                    var temp = response.current_observation.feelslike_string;
 
-                    var list = response.list
+                    // store weather icon
+                    var weatherIcon = response.current_observation.icon_url;
 
-                    for (var i = 0; i < list.length; i++) {
-                        console.log(list[i].temp.max);
+                    // show weather html
+                    $('weather').addClass('animated slideInUp');
+                    $('#weather').html('<img id="weather-icon" src="' + weatherIcon + '" alt="weather icon">');
+                    $('#weather').append('<h4>Current Temp:</h4>');                    
+                    $('#weather').append('<h3 id="temp">' + temp + '</h3>');
+                    
 
-                        $("#weather").append(list[i].temp.max);
-                    }
+
                 });
 
         }
